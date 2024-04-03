e358fbffdfeded1260267455f19489cff714cd80
AhanaMohanta <125819062+Amohanta0019@users.noreply.github.com>
2024-04-03 18:45:39 +0000

    data types

---
+++ 01_Basics/03_data-types.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as per newer version
+
+// alert(3+3)  //gives error as  we are using node js not browser
+
+let name = "ahana"
+let age = 20
+let isLoggedIn = false
+
+/* 
+number --> 2^53 appox limit
+bigint --> for very large number
+string --> '' or ""
+boolean --> true or false
+null --> standalone value as well as data-type
+representation of empty value
+undefined --> variable is declared but value not assigned
+symbol --> to find uniqueness
+object -->
+*/
+
+console.log( typeof("ahana")) // string
+console.log( typeof(undefined)) // undefined
+console.log( typeof(null)) // object
+
