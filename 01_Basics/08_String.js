60095f2769cc82a768843296365ff76c4a70172d
AhanaMohanta <125819062+Amohanta0019@users.noreply.github.com>
2024-04-06 16:28:23 +0000

    string in js

---
+++ 01_Basics/08_String.js
@@ -0,0 +1,24 @@
+const name = "ahana"
+const repoCnt  = 50
+console.log(name+repoCnt+" value");
+//ahana50 value
+
+console.log(`hello my name is ${name} and my repository count is ${repoCnt}`);
+//hello my name is ahana and my repository count is 50
+
+const gameName = new String("GTA5")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
