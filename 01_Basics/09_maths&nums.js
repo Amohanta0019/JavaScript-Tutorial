4bb71d105d90105d0fe479fd3a5fe2962a04848f
AhanaMohanta <125819062+Amohanta0019@users.noreply.github.com>
2024-04-07 06:31:20 +0000

    maths in js

---
+++ 01_Basics/09_maths&nums.js
@@ -0,0 +1,40 @@
+const score = 400
+// console.log(score);//400
+const balance = new Number(900) //object 
+// console.log(balance); //[Number: 900]
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(3));//900.000  --> no. of 0 after decimal 
+
+const num = 123.456789
+// console.log(num.toPrecision(4)); //123.5 as it rounds off .45  to .50 so 123.5
+
+const hundred = 1000000000000
+// console.log(hundred.toLocaleString());//1,000,000,000,000 --> to count 0's  in us standerd (by default)
+// console.log(hundred.toLocaleString('en-IN'));//10,00,00,00,00,000 --> to count 0's in indian standerd
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
