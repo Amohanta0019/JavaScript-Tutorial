a1f3c2eedc94a35cf96e02d71a0f37f0309f32cd
AhanaMohanta <125819062+Amohanta0019@users.noreply.github.com>
2024-04-09 18:34:12 +0000

    array basics in js

--- 02_Arrays/01_Array-basics.js
+++ 02_Arrays/01_Array-basics.js
@@ -15,9 +15,28 @@ arr.push(54)//[ 1, 3, 4, 67, 79, 54 ]
 arr.pop()//[ 1, 3, 4, 67, 79 ]
 arr.unshift(7)//[ 7, 1, 3, 4, 67, 79 ]
 arr.shift()//[ 1, 3, 4, 67, 79 ]
+// console.log(arr);
 
+// console.log(arr.includes(10));//false
+// console.log(arr.indexOf(10));//-1 means there is no element
 
-console.log(arr);
+const newArr = arr.join()
+// console.log(arr);//[ 1, 3, 4, 67, 79 ]
+// console.log(newArr);//1,3,4,67,79
+// console.log(typeof newArr);//string
+
+//slice & splice
+
+console.log("a", arr);
+//last index not included & original array remains same after slicing
+const myn1 = arr.slice(1,3)
+console.log(myn1);//[ 3, 4 ]
+console.log("b", arr);//b [ 1, 3, 4, 67, 79 ]
+console.log();
+//last range included & original array changed , only remaining portion included
+const myn2 = arr.splice(1,3)
+console.log(myn2); //[ 3, 4, 67 ]
+console.log("c", arr);//c [ 1, 79 ]
 
 
 
