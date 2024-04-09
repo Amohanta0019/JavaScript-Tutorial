027b9ce98697be7f8f33b1c82430d484cc412dac
AhanaMohanta <125819062+Amohanta0019@users.noreply.github.com>
2024-04-09 04:10:20 +0000

    dates in js

---
+++ 01_Basics/10_dates.js
@@ -0,0 +1,29 @@
+//dates
+
+let myDate = new Date()
+// console.log(myDate);//2024-04-09T04:03:39.829Z
+
+// console.log(myDate.toString());//Tue Apr 09 2024 04:04:02 GMT+0000 (Coordinated Universal Time)
+
+// console.log(myDate.toDateString());//Tue Apr 09 2024
+
+// console.log(myDate.toLocaleString());//4/9/2024, 4:06:31 AM
+
+// console.log(typeof(myDate));//object
+
+let myDate1 = new Date(2024, 0,23)
+console.log(myDate1);//2024-01-23T00:00:00.000Z
+
+
+
+
+
+
+
+
+
+
+
+
+
+
