2f2130846a0febaab6d7642678b2ebfd89fa88a1
AhanaMohanta <125819062+Amohanta0019@users.noreply.github.com>
2024-04-09 15:52:06 +0000

    arrays

---
+++ 02_Arrays/01_Array-basics.js
@@ -0,0 +1,33 @@
+//array
+
+// const myArr = [0,1,2,3.3, true, 'ahana', ]
+// console.log(myArr);//[ 0, 1, 2, 3.3, true, 'ahana' ]
+// in js arrays are resizeable
+
+// console.log(myArr[0]);//0
+
+const arr = new Array(1,3,4,67,79);
+// console.log(arr);
+
+//array methods
+
+arr.push(54)//[ 1, 3, 4, 67, 79, 54 ]
+arr.pop()//[ 1, 3, 4, 67, 79 ]
+arr.unshift(7)//[ 7, 1, 3, 4, 67, 79 ]
+arr.shift()//[ 1, 3, 4, 67, 79 ]
+
+
+console.log(arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
