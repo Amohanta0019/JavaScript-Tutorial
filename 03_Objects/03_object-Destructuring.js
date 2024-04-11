b129ee021d71d988ceaff91016920fcf739cb49a
AhanaMohanta <125819062+Amohanta0019@users.noreply.github.com>
2024-04-11 06:40:03 +0000

    object destructuring

--- 03_Objects/03_object-Destructuring.js
+++ 03_Objects/03_object-Destructuring.js
@@ -1,9 +1,16 @@
 const course = {
     cmane : "js in hindi",
-    
+    price : 999,
+    instructor : "abc"
 }
 
+// course.instructor
 
+// const {instructor} = course
+// console.log(instructor);//abc
+
+const {instructor : i} = course // giving short name
+// console.log(i);//abc
 
 
 
